24b363a4a66ed05454cc1f4afee9b1f21a0ee7d2
AAdams <aadams28@mail.greenriver.edu>
2020-04-23 19:28:55 -0700

    Finalized ifthen

--- Python/ifthen.py
+++ Python/ifthen.py
@@ -2,12 +2,14 @@
 # If/then example
 
 def main():
-  x = 200
+  x = 100
   y = 100
   if x < y:
     print("x is less than y")
   elif x > y:
     print("x is greater than y")
+  else:
+    print("x is equal to y")
 
 
 
