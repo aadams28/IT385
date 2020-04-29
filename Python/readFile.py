6b43e1fb33dde23cbad87050871cc323604d9263
AAdams <aadams28@mail.greenriver.edu>
2020-04-29 15:08:06 -0700

    Finished reading

--- Python/readFile.py
+++ Python/readFile.py
@@ -6,4 +6,15 @@ testFile = open("testfile.txt", "r")
 print(testFile.read(12))
 testFile.close()
 
+# read file line by line
+testFile = open("testfile.txt", "r")
+for line in testFile:
+  if line.startswith("A"):
+    print(line)
+testFile.close()
+
+
+# Read file using WITH statement
+with open("testfile.txt", "r") as testFile:
+  print(testFile.read())
 
