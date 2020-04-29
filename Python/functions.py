d900e250e7e88f4017fb063037e970aede0edde3
AAdams <aadams28@mail.greenriver.edu>
2020-04-28 19:47:34 -0700

    Cube function

--- Python/functions.py
+++ Python/functions.py
@@ -10,16 +10,20 @@ def addNums(val1, val2):
   print(val1 + val2)
 
 
-
-
+# function that returns a value
+def cube(x):
+  return x * x * x
+  
 # Call function
 #func1()
 #print(func1())
 #print(func1)
 
 
-addNums(5, 6)
-addNums(val2 = 11, val1= 9)
+#addNums(5, 6)
+#addNums(val2 = 11, val1= 9)
+
+print(cube(5))
 
 
 
