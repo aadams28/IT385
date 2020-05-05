5311c32b3763e002824441622ebe5dfee0b51b51
AAdams <aadams28@mail.greenriver.edu>
2020-05-04 20:30:25 -0700

    Finished GetUptime

---
+++ Python/GetUptime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Expect script using pxssh
+
+from pexpect import pxssh
+
+# create session and login
+s = pxssh.pxssh()
+s.login('192.168.0.111', 'justincase', 'Password01')
+print('SSH loging succesful')
+
+# Send command
+s.sendline('uptime')
+s.prompt()
+print(s.before)
+
+
+# logout
+s.logout()
+
