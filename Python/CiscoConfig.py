40af492b781283dba5d518995e83edcd267a1d36
AAdams <aadams28@mail.greenriver.edu>
2020-06-06 20:30:13 -0700

    Finished CiscoConfig

--- Python/CiscoConfig.py
+++ Python/CiscoConfig.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
+#Importing the necessary modules needed(In this case pexpect)
 import pexpect
 
-switch_ip = "192.168.0.11"
-switch_un = ""
-switch_pw = ""
-switch_port = "g02"
-
+#Establishing SSH connection to the Cisco device(for multiple devices I can add
+#more pexpect.spawns and use the same commands)
 child = pexpect.spawn('ssh cisco@192.168.0.11')
+
+#Setting how long the terminal should wait before ending the session
 child.timeout = 4
+
+#Sending the commands I need to configure basic settings on the device
 child.expect('Password:')
 child.sendline('cisco')
 child.expect('IT385-CSR1#')
@@ -16,11 +18,22 @@ child.sendline('config t')
 child.expect('IT385-CSR1\(config\)#')
 child.sendline('int g02')
 child.expect('IT385-CSR1\(config-if\)#')
-child.sendline('ip address 10.15.14.1 255.255.0.0')
+child.sendline('ip address 10.15.13.1 255.255.0.0')
 child.expect('IT385-CSR1\(config-if\)#')
 child.sendline('no shutdown')
 child.expect('IT385-CSR1\(config-if\)#')
 child.sendline('exit')
 child.expect('IT385-CSR1\(config\)#')
-child.sendline('wr mem')
+child.sendline('no ip domain-lookup')
 child.expect('IT385-CSR1\(config\)#')
+child.sendline('banner motd %No Unauthorized Access!%')
+child.expect('IT385-CSR1\(config\)#')
+child.sendline('service password-encryption')
+child.expect('IT385-CSR1\(config\)#')
+child.sendline('hostname R1')
+child.expect('R1\(config\)#')
+child.sendline('wr mem')
+child.expect('R1\(config\)#')
+
+#This will provide visual feedback that the configuration was successful
+print('Router/Switch is configured')
